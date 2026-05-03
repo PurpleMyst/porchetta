efe7429bebe60c255e0598e54f40fce86de2ab83
PurpleMyst <PurpleMyst@users.noreply.github.com>
2026-05-03 17:12:43 +0200

    feat: normalize line endings to LF

--- src/engine/capture.rs
+++ src/engine/capture.rs
@@ -5,6 +5,18 @@ use gix::ObjectId;
 use super::path_util::to_tree_path;
 use crate::store::PorchettaStore;
 
+/// Normalize line endings in the content to LF. If the content is not valid UTF-8, it is returned as-is.
+fn normalize_line_endings(content: Vec<u8>) -> Vec<u8> {
+    // XXX: Heuristic-y but in my own usage this works; we can add a topic-level is_binary callback
+    // later if the need arises. ¯\_(ツ)_/¯
+    let Some(content_str) = std::str::from_utf8(&content).ok() else {
+        // If the content is not valid UTF-8, return it as-is.
+        return content;
+    };
+    // Normalize line endings to LF.
+    content_str.replace("\r\n", "\n").into_bytes()
+}
+
 /// Read files from disk, apply the `to_repo` transform, and write them into a new git tree.
 ///
 /// # Errors
@@ -22,7 +34,7 @@ pub fn snapshot_topic(
         let relative_path = to_tree_path(file.strip_prefix(topic_base)?);
         let content =
             std::fs::read(file).with_context(|| format!("Failed to read file '{file}'"))?;
-        let content = to_repo(&relative_path, &content)?;
+        let content = normalize_line_endings(to_repo(&relative_path, &content)?);
         let blob_oid = store.write_blob(&content)?;
         editor.upsert(&relative_path, gix::objs::tree::EntryKind::Blob, blob_oid)?;
     }
